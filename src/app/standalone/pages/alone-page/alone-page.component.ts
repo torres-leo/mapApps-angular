08ee923797ec1e94995cd557bd7b7fc6448c3d70
torres-leo <leojts24@gmail.com>
2023-05-13 16:38:09 -0600

    add standalone component

---
+++ src/app/standalone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.scss'],
+})
+export class AlonePageComponent {}
